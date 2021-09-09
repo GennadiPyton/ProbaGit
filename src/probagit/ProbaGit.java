d949b1ba34ec3e89d9f96d3e28e8390318bc3714
Your Name <you@example.com>
2021-09-09 18:53:24 +0300

    Вторая версия проекта

--- src/probagit/ProbaGit.java
+++ src/probagit/ProbaGit.java
@@ -17,6 +17,7 @@ public class ProbaGit {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Первая версия програмы.");
+        System.out.println("Вторая версия програмы.");
     }
     
 }
